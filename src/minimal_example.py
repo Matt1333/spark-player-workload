e81277c3804c245de7eb99bcbcd9164a4fb2e580
erwan-oss <erwan.touvron@edu.ece.fr>
2025-12-11 14:13:44 +0100

    Update docker-compose and minimal_example

---
+++ src/minimal_example.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+
+def main():
+    spark = (
+        SparkSession.builder
+        .appName("MinimalSparkExample")
+        .getOrCreate()
+    )
+
+    data = [("Matt", 1), ("Erwan", 2), ("Teacher", 3)]
+    df = spark.createDataFrame(data, ["name", "value"])
+
+    print("Spark session is running, tiny DataFrame below:")
+    df.show()
+
+    spark.stop()
+
+
+if _name_ == "_main_":
+    main()
